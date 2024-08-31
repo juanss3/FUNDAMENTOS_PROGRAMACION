a5edd5b24c37ffac13f98548cbcaf03397970aae
juanss3 <juanmanuelss3@gmail.com>
2024-08-31 03:59:11 -0500

    mejoras la proyecto

--- 16.proyecto_final/main.py
+++ 16.proyecto_final/main.py
@@ -1,55 +1,49 @@
 import os
-productos = [
-    {"nombre": "anillo", "precio": 300, "stock": 10},
-    {"nombre": "pulcera", "precio": 150, "stock": 20},
-    {"nombre": "reloj", "precio": 100, "stock": 15},
-    {"nombre": "aretes", "precio": 600, "stock": 5},
-]
 
-def limpiar_terminal():
-    if os.name == "nt": os.system("cls")#windows
-    else: os.system('clear') #linux o mac
+
+productos = [
+    {"nombre": "anillo", "precio": 350, "stock": 8},
+    {"nombre": "pulsera", "precio": 180, "stock": 25},
+    {"nombre": "reloj", "precio": 120, "stock": 12},
+    {"nombre": "aretes", "precio": 650, "stock": 7},
+    {"nombre": "collar", "precio": 320, "stock": 9},
+    {"nombre": "broche", "precio": 170, "stock": 18},
+]
 
 carrito = []
 
+def limpiar_terminal():
+    if os.name == "nt": os.system("cls")  # Windows
+    else:
+        os.system("clear")  # Linux
+
+
 def productos_disponibles():
     for producto in productos:
-        nombre = producto["nombre"] 
+        nombre = producto["nombre"]
         precio = producto["precio"]
         cantidad = producto["stock"]
-        
-        
-        print()
-
-        print(f"PRODUCTO: {nombre}   PRECIO: ${precio}   CANTIDAD: {cantidad}")
-
-
+        print(f"\nPRODUCTO: {nombre}   PRECIO: ${precio}   CANTIDAD: {cantidad}")
 
 def add_products(nombre, cantidad):
     limpiar_terminal()
-    print()
-    print("---- producto agregado-----")
+    print("\n---- producto agregado-----")
     try:
-        producto = next((p for p in productos if p ["nombre"] == nombre), None) #codigo optimizado
-        
+        producto = next((p for p in productos if p["nombre"] == nombre), None)
         if producto:
-            if producto ["stock"] >= cantidad:
-                
-                item_carrito = next((c for c in carrito if c ["nombre"] == nombre),None)
-                
+            if producto["stock"] >= cantidad:
+                item_carrito = next((c for c in carrito if c["nombre"] == nombre), None)
                 if item_carrito:
-                    item_carrito ["stock"] += cantidad
+                    item_carrito["stock"] += cantidad
                 else:
-                    carrito.append({"nombre": nombre,"precio":producto ["precio"],  "stock": cantidad})# append sirve para añadir una lista en la variable carrito
-                    producto ["stock"] -= cantidad
+                    carrito.append({"nombre": nombre, "precio": producto["precio"], "stock": cantidad})
+                    producto["stock"] -= cantidad
             else:
                 print(f"no hay suficiente stock para el producto {nombre}")
         else:
             print(f"el producto {nombre} no existe")
     except ValueError:
         print("*** OPCION INVALIDA ***")
-
-
 
 def mostrar_carrito():
     print("----------------------- Carrito ------------------------------")
@@ -59,65 +53,73 @@ def mostrar_carrito():
             precio = producto["precio"]
             cantidad = producto["stock"]
             subtotal = precio * cantidad
-            
-            print()
-            print(f"PRODUCTO: {nombre}   PRECIO: ${precio}   CANTIDAD: {cantidad}   SUBTOTAL: ${subtotal}")
+            print(f"\nPRODUCTO: {nombre}   PRECIO: ${precio}   CANTIDAD: {cantidad}   SUBTOTAL: ${subtotal}")
+        print("\n------------------------------------------------------------\n")
     else:
-        print()
-        print("El carrito está vacío")
+        print("\nEl carrito está vacío")
 
-
-def precio_total(): ##################################################################################
-    total_carrito = sum(productos["precio"] * productos["stock"]for productos in carrito)
-    
-    print(f"el valor del carrito es: $({total_carrito})")
-
+def precio_total():
+    total_carrito = sum(productos["precio"] * productos["stock"] for productos in carrito)
+    print(f"Precio Total: ${total_carrito}")
 
 def main():
     try:
         while True:
+            print("----------Opciones-----------") 
+            print("1. mostrar productos")
+            print("2. agregar producto al carrito")
+            print("3. mostrar carrito")
+            print("4. finalizar compra")
+            print("5. salir")
             
-                print("----------Opciones-----------") 
-                print("1. mostrar productos")
-                print("2. agregar producto al carrito")
-                print("3. mostrar carrito")
-                print("4. finalizar compra")
-                print("5. salir")
-            
-                opcion = int(input("ingrese una opcion: "))
-                if opcion == 1:
-                    limpiar_terminal()
-                    print("-------- Productos Disponibles -------")
-                    productos_disponibles()
-                    
-                    
-                elif opcion == 2:
-                    print("-------- Agregue producto -------")
-                    nombre = input("nombre de producto: ")
-                    cantidad = int(input("cantidad de productos: "))
-                    add_products(nombre,cantidad)
-                    
-                elif opcion == 3:
+            opcion = int(input("ingrese una opcion: "))
+            if opcion == 1:
+                limpiar_terminal()
+                print("-------- Productos Disponibles -------")
+                productos_disponibles()
+
+            elif opcion == 2:
+                limpiar_terminal()
+                print("-------- Agregue producto -------")
+                nombre = input("nombre de producto: ")
+                cantidad = int(input("cantidad de productos: "))
+                add_products(nombre, cantidad)
+
+            elif opcion == 3:
+                mostrar_carrito()
+                precio_total()
+
+            elif opcion == 4:
+                if carrito:
                     mostrar_carrito()
                     precio_total()
-                    
-                elif opcion == 4:
-                    print()
-                    mostrar_carrito()
-                    print()# ################################################# terminar ############################
-                    print("para pagar pulse 1")
-                    
-                    break
-                elif opcion == 5:
-                    print("Saliendo . . .")
-                    break
+                    print("\n1. pagar ahora mismo")
+                    print("2. modificar el carrito")
+                    try:
+                        opcion = int(input("elija una opción: "))
+                        if opcion == 1:
+                            limpiar_terminal()
+                            print("-----------Su compra ha sido realizada-----------")
+                            mostrar_carrito()
+                            precio_total()
+                            break
+                        else:
+                            limpiar_terminal()
+                            main()
+                    except ValueError:
+                        print("*******OPCION INAVLIDA*********")
+                        
                 else:
-                    print("*************opcion no valida*************")
+                    print("\nNO HAY PRODUCTOS EN EL CARRITO")
+
+            elif opcion == 5:
+                print("Saliendo . . .")
+                break
+
+            else:
+                print("*************opcion no valida*************")
     except ValueError:
-        print()
-        print("*** OPCION INVALIDA ***")
-        print()
+        print("\n*** OPCION INVALIDA ***")
         main()
-    
 
 main()
